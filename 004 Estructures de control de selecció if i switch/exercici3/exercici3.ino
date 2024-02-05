356dbeab1db467ad82fa79632222f2f4051d9e53
DIDACPALAUSALAS <157710403+DIDACPALAUSALAS@users.noreply.github.com>
2024-02-05 08:58:53 +0100

    dídac palau

--- 004 Estructures de control de selecció if i switch/exercici3/exercici3.ino
+++ 004 Estructures de control de selecció if i switch/exercici3/exercici3.ino
@@ -10,10 +10,18 @@ void setup()              //
 {
   Serial.begin(9600);     // 
 
-  if (tempAigua >= 100)   // 
+  if (tempAigua > 100)   // 
   {
     Serial.print("Aigua bullint!");
   } 
+  else if (tempAigua == 100)   // 
+  {
+    Serial.print("Aigua a 100ºC");
+  }
+  else if (tempAigua >= 90)   // 
+  {
+    Serial.print("Aigua apunt de bullir");
+  }
   else                    // 
   {
     Serial.print("Aigua encara no bull");
