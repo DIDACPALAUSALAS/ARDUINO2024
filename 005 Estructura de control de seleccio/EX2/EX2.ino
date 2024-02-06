9d5118a49181442d07b6d5883f9543af6ce959fd
DIDACPALAUSALAS <157710403+DIDACPALAUSALAS@users.noreply.github.com>
2024-02-06 08:31:43 +0100

    DÍDAC PALAU

---
+++ 005 Estructura de control de seleccio/EX2/EX2.ino
@@ -0,0 +1,20 @@
+int comptar = 11;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Ara comptare de 0 a ");
+  Serial.println(comptar);
+  
+  for (int i = 0; i <= comptar; i++) {
+    Serial.print(i);
+    
+    // 
+    if (i < comptar) {
+      Serial.print("-");
+    }
+  }
+}
+
+void loop() {
+}
+
